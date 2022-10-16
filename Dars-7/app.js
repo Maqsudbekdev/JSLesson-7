3a8504cb95ac133a4850956c7f9f9605442fdacd
Maqsudbek <115130682+Maqsudbekdev@users.noreply.github.com>
2022-10-16 14:54:52 +0500

    Add files via upload

---
+++ Dars-7/app.js
@@ -0,0 +1,18 @@
+// btn.addEventListener('click',()=>{
+//     console.log(inp.value);
+//     box.style.background = inp.value;
+// })
+//  inp.addEventListener('click',()=>{
+//     // box.style.borderRadius = inp.value + "px"
+     
+//     box.style.width = inp.value*3 + "px"
+//     box.style.height = inp.value*3 + "px"
+
+//  })
+
+
+
+sel.addEventListener('change',()=>{
+    console.log(sel.value);
+    box.style.background = sel.value
+})
